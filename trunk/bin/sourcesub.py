57589986cd16d2fe42dd1a747197332dc7745685
Ryan Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2016-08-05 22:06:19 +0000

    Added arguments to sourcesub.py

--- trunk/bin/sourcesub.py
+++ trunk/bin/sourcesub.py
@@ -5,119 +5,153 @@ import socket
 import lsc
 import numpy as np
 
-host = socket.gethostname()
-if host in ['intern1.lco.gtn']:
-    os.chdir('/home/rkirkpatrick/seatide/tmp/')
-elif host in ['astrointern.lco.gtn']:
-    os.chdir('/home/dguevel/snwd/tmp')
+description="Modified version of seatide_imagesub.py used to automatically "\
+            "create and reduce fake source difference images."
+usage = "%(prog)s [-e epoch -n objname -m magnitude]"
 
-try:
-    hostname,username, passwd, database = lsc.mysqldef.getconnection('lcogt2')
-    db = lsc.mysqldef.dbConnect(hostname, username, passwd, database)
-except:
-    print 'Error: Could not connect to database'
+# Parse arguments
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=description, usage=usage)
+    parser.add_argument("-n", "--name", dest="name", default='', type=str,
+                        help='-n object name   \t [%(default)s]')
+    parser.add_argument("-m", "--magnitude", nargs="+", default=[], help=
+                        "Choose data from a specific magnitude", type=float)
+    parser.add_argument("-e", "--epoch", dest="epoch", default='', type=str,
+                        help='epoch to search for data \t [%(default)s]')
 
-# query photlco for available images
-query  = '''SELECT pr.objname, p.targetid, p.dayobs, p.filepath, p.wcs, p.filename, p.instrument '''
-query += '''FROM photlco AS p, photlcoraw AS pr '''
-query += '''WHERE pr.filename = p.filename '''
-query += '''AND pr.propid in ('CON2015B-002','CON2016A-004') '''
-query += '''AND p.filetype = 1 '''
-query += '''AND p.quality = 127 '''
-query += '''AND p.filename LIKE '%e91%' '''
-query += '''AND p.z1 != 9999 '''
-query += '''ORDER by p.dateobs, p.ut DESC '''
-objects = lsc.mysqldef.sqlquery(db,query)
+    args = parser.parse_args()
+    _name = args.name
+    _magnitude = args.magnitude
+    _epoch = args.epoch
 
-#print objects
-#raw_input()
+    # Change directory
+    host = socket.gethostname()
+    if host in ['intern1.lco.gtn']:
+        os.chdir('/home/rkirkpatrick/seatide/tmp/')
+    elif host in ['astrointern.lco.gtn']:
+        os.chdir('/home/dguevel/snwd/tmp')
+    # Try to connect to database
+    try:
+        hostname,username, passwd, database = lsc.mysqldef.getconnection('lcogt2')
+        db = lsc.mysqldef.dbConnect(hostname, username, passwd, database)
+    except:
+        print 'Error: Could not connect to database'
 
-for i,row in enumerate(objects):
+    # query photlco for available images
+    query  = '''SELECT pr.objname, p.targetid, p.dayobs, p.filepath, p.wcs, p.filename, p.instrument '''
+    query += '''FROM photlco AS p, photlcoraw AS pr '''
+    query += '''WHERE pr.filename = p.filename '''
+    query += '''AND pr.propid in ('CON2015B-002','CON2016A-004') '''
+    query += '''AND p.filetype = 1 '''
+    query += '''AND p.quality = 127 '''
+    query += '''AND p.filename LIKE '%e91%' '''
+    query += '''AND p.z1 != 9999 '''
+    if '-' not in str(_epoch):
+        query += ' AND p.dayobs = {0} '.format(_epoch)
+    else:
+        epoch1, epoch2 = _epoch.split('-')
+        query += ' AND p.dayobs >= {0} AND p.dayobs <= {1} '.format(epoch1,epoch2)
 
-    # pull new image from database
-    objname = row['objname'] # name of object
-    targetid = row['targetid']
-    dayobs = row['dayobs']
-    instrument = row['instrument']
+    if _name != '':
+        query += ' AND p.objname = "{0}" '.format(_name)
 
-    print ''
-    print '''Checking image from %(dayobs)s for target %(objname)s.''' %row
+    query += '''ORDER by p.dateobs, p.ut DESC '''
+    objects = lsc.mysqldef.sqlquery(db,query)
 
-    # has this image already been differenced?
-    fname = row['filename'].replace('e91','e93')
-    fpath = row['filepath']
-    diffname = fname.replace('.fits','.diff.fits')
-    if os.path.exists(fpath+diffname):
-        print "Clearing DB and deleting already differenced."
-        lsc.mysqldef.deleteredufromarchive(diffname,"photlcoraw")
-        lsc.mysqldef.deleteredufromarchive(diffname,"photlco")
+    if _magnitude == []:
+        _magnitude = np.arange(14,20.5,0.5)
+
+    #print objects
+    #raw_input()
+
+    for i,row in enumerate(objects):
+
+        # pull new image from database
+        objname = row['objname'] # name of object
+        targetid = row['targetid']
+        dayobs = row['dayobs']
+        instrument = row['instrument']
+
+        print ''
+        print '''Checking image from %(dayobs)s for target %(objname)s.''' %row
+
+        # has this image already been differenced?
+        fname = row['filename'].replace('e91','e93')
+        fpath = row['filepath']
+        diffname = fname.replace('.fits','.diff.fits')
+        if os.path.exists(fpath+diffname):
+            print "Clearing DB and deleting already differenced."
+            lsc.mysqldef.deleteredufromarchive(diffname,"photlcoraw")
+            lsc.mysqldef.deleteredufromarchive(diffname,"photlco")
 
 
-    # fix WCS if failed?
-    if row['wcs'] != 0:
-        print '''No WCS, attempting to solve.'''
-        command = 'lscloop.py -n ' + objname + ' -e ' + dayobs + ' -s wcs '
-        print command
-        os.system(command)
-
-    # is there an LCOGT template?
-    query  = '''SELECT filename, filepath, dayobs ,psf '''
-    query += '''FROM photlco '''
-    query += '''WHERE targetid=%s ''' %targetid
-    query += '''AND instrument like '%fl%' '''
-    query += '''AND filetype = 4 '''
-    query += '''AND dayobs != '%s' ''' %row['dayobs']
-    query += '''AND filename LIKE '%e91%' '''
-    query += '''ORDER BY dateobs, ut LIMIT 1 '''
-    fileinfo = lsc.mysqldef.sqlquery(db,query)
-    templatedate =''
-    # if LCOGT template exists:
-    if len(fileinfo)>0:
-        templatesource = 'LCOGT'
-        templatedate = fileinfo[0]['dayobs']
-        print ''' Found LCOGT template from %s.''' %templatedate
-        # does the template have a good PSF?
-        if fileinfo[0]['psf'] == 'X':
-            print ''' Making PSF for the template:'''
-            command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' --filetype=4 -s psf'
+        # fix WCS if failed?
+        if row['wcs'] != 0:
+            print '''No WCS, attempting to solve.'''
+            command = 'lscloop.py -n ' + objname + ' -e ' + dayobs + ' -s wcs '
             print command
             os.system(command)
-    else:
-        # is there an LCOGT image that could be used as a template?       
-        query  = '''SELECT filename, filepath, dayobs, psf '''
+
+        # is there an LCOGT template?
+        query  = '''SELECT filename, filepath, dayobs ,psf '''
         query += '''FROM photlco '''
         query += '''WHERE targetid=%s ''' %targetid
         query += '''AND instrument like '%fl%' '''
-        query += '''AND filetype = 1 '''
-        query += '''AND dayobs != %s ''' %dayobs
+        query += '''AND filetype = 4 '''
+        query += '''AND dayobs != '%s' ''' %row['dayobs']
         query += '''AND filename LIKE '%e91%' '''
-        query += '''ORDER BY dayobs, ut LIMIT 1 '''
+        query += '''ORDER BY dateobs, ut LIMIT 1 '''
         fileinfo = lsc.mysqldef.sqlquery(db,query)
-        print fileinfo
+        templatedate =''
+        # if LCOGT template exists:
         if len(fileinfo)>0:
-            print ''' Found earlier LCOGT image to use as template. Will convert to template:'''
             templatesource = 'LCOGT'
             templatedate = fileinfo[0]['dayobs']
+            print ''' Found LCOGT template from %s.''' %templatedate
+            # does the template have a good PSF?
             if fileinfo[0]['psf'] == 'X':
-                print ''' Oh, this file doesn't have a PSF. Will try to make one with FHWM 7:'''
-                command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s psf --fwhm=7'
+                print ''' Making PSF for the template:'''
+                command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' --filetype=4 -s psf'
+                print command
                 os.system(command)
-                print ''' Now back to converting it to template:'''
-            command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s template'
-            print command
-            os.system(command)
-            print ''' Making PSF for the template:'''
-            command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' --filetype=4 -s psf'
+        else:
+            # is there an LCOGT image that could be used as a template?
+            query  = '''SELECT filename, filepath, dayobs, psf '''
+            query += '''FROM photlco '''
+            query += '''WHERE targetid=%s ''' %targetid
+            query += '''AND instrument like '%fl%' '''
+            query += '''AND filetype = 1 '''
+            query += '''AND dayobs != %s ''' %dayobs
+            query += '''AND filename LIKE '%e91%' '''
+            query += '''ORDER BY dayobs, ut LIMIT 1 '''
+            fileinfo = lsc.mysqldef.sqlquery(db,query)
+            print fileinfo
+            if len(fileinfo)>0:
+                print ''' Found earlier LCOGT image to use as template. Will convert to template:'''
+                templatesource = 'LCOGT'
+                templatedate = fileinfo[0]['dayobs']
+                if fileinfo[0]['psf'] == 'X':
+                    print ''' Oh, this file doesn't have a PSF. Will try to make one with FHWM 7:'''
+                    command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s psf --fwhm=7'
+                    os.system(command)
+                    print ''' Now back to converting it to template:'''
+                command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s template'
+                print command
+                os.system(command)
+                print ''' Making PSF for the template:'''
+                command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' --filetype=4 -s psf'
+                print command
+                os.system(command)
+
+        if templatedate != '':
+            print ''' Performing cosmic-ray rejection on template:'''
+            command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s cosmic --filetype 4'
             print command
             os.system(command)
 
-    if templatedate != '':
-        print ''' Performing cosmic-ray rejection on template:'''
-        command = 'lscloop.py --obstype e91 -n ' + objname + ' -e ' + templatedate + ' -s cosmic --filetype 4'
-        print command
-        os.system(command)
 
-        for i in np.arange(14,20.5,0.5):
+        for i in _magnitude:
             inmag = i
 
             # create fake source images
@@ -136,7 +170,7 @@ for i,row in enumerate(objects):
             command = 'lscloop.py --obstype e93.fits -n ' + objname + ' -e ' + dayobs + ' -s cosmic'
             print command
             os.system(command)
- 
+
             # run image subtraction
             print ''' Running image subtraction:'''
             command = 'lscloop.py --obstype e93.fits e91 -n ' + objname + ' -e ' + dayobs + ' --tempdate ' + templatedate + ' -s diff --normalize t -T ' + instrument[0:2]
@@ -196,4 +230,8 @@ for i,row in enumerate(objects):
     os.system('rm -f tmpcoo')
 
 
-#CREATE TABLE magcomparison( id bigint(20) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, targetid bigint(20) NOT NULL, filepath text DEFAULT NULL, filename varchar(100) DEFAULT NULL, dayobs text DEFAULT NULL, objname text DEFAULT NULL, inmag double DEFAULT 9999, diffmag double DEFAULT 9999, outlier boolean DEFAULT false);
+    #CREATE TABLE magcomparison( id bigint(20) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        #  targetid bigint(20) NOT NULL, filepath text DEFAULT NULL,
+        #  filename varchar(100) DEFAULT NULL, dayobs text DEFAULT NULL,
+        #  objname text DEFAULT NULL, inmag double DEFAULT 9999,
+        #  diffmag double DEFAULT 9999, outlier boolean DEFAULT false);
